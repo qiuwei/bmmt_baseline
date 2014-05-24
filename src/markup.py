0aa169fd2773544c681c204c9b028c8a49ee9232
Wei Qiu <wei@qiu.es>
2014-05-24 14:49:26 +0200

    add result for tuning with pro

--- src/markup.py
+++ src/markup.py
@@ -9,17 +9,20 @@ def readin_term(path):
     """
     term_dict = {}
     with open(path, 'r') as term_file:
-        for line in term_file
+        for line in term_file:
             determ, enterm = line.strip().split('\t')
             term_dict[determ] = enterm
     return term_dict
 
-def process_de_str():
+def process_de_str(destr, term_dict, depattern):
     """
     preprocess the input German string to add the xml markup
     """
 
-
+def build_de_pattern(term_dict):
+    """
+    build a regualr expression pattern which matches all de phrases in the term_dict
+    """
 def process_de():
     """
     preprocess the input German file to add the xml markup
